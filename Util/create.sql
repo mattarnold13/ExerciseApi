f557b78d566acc41a6863b83d473a6ccc764cdd2
mattarnold13.com <mattnc13@gmail.com>
2023-09-24 19:42:04 -0400

    first commit

---
+++ Util/create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE ExerciseList (
+	ID INT NOT NULL AUTO_INCREMENT,
+    ExerciseType char(25) NOT NULL,
+    ExerciseDate DATE,
+    ExerciseTime NUMERIC(4,0),
+    ExerciseLevel NUMERIC(4,0),
+    ExerciseReps NUMERIC(2,0),
+    ExerciseSets NUMERIC(2,0),
+    ExerciseWeight NUMERIC (3,0),
+    ExerciseNotes VARCHAR(100),
+    ExercisePerson Numeric(3,0),
+    primary key (id)
+);
